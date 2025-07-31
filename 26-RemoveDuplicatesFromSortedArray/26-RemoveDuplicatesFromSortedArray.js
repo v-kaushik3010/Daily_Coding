2448d0d49838ff7942bb33185f736b147c8b0fb1
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-07-31 18:06:30 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,13 @@
+// Last updated: 7/31/2025, 6:06:17 PM
+var removeDuplicates = function(nums) {
+    let i = 1;
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i - 1]) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+
+    return i;    
+};
