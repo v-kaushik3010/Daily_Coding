be070cda8ed9b938ed1454589e1c319c7a8f4d67
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-07-31 18:03:10 +0530

    Create 2998-CountSymmetricIntegers

---
+++ 2998-CountSymmetricIntegers/2998-CountSymmetricIntegers.java
@@ -0,0 +1,19 @@
+// Last updated: 7/31/2025, 6:02:57 PM
+class Solution {
+
+        public int countSymmetricIntegers(int low, int high) {
+                int res = 0;
+                        for (int a = low; a <= high; ++a) {
+                                    if (a < 100 && a % 11 == 0) {
+                                                    res++;
+                                                                } else if (1000 <= a && a < 10000) {
+                                                                                int left = a / 1000 + (a % 1000) / 100;
+                                                                                                int right = (a % 100) / 10 + (a % 10);
+                                                                                                                if (left == right) {
+                                                                                                                                    res++;
+                                                                                                                                                    }
+                                                                                                                                                                }
+                                                                                                                                                                        }
+                                                                                                                                                                                return res;
+                                                                                                                                                                                    }
+                                                                                                                                                                                    }
