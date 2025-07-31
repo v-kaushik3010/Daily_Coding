e0b23931d0e638c195d5e87874eb1abf4b425dbb
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-07-31 18:03:18 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 7/31/2025, 6:03:05 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
