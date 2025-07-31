aa9ff487c18084b886145b714c27826a05b86897
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-07-31 18:04:03 +0530

    Create 2262-SolvingQuestionsWithBrainpower

---
+++ 2262-SolvingQuestionsWithBrainpower/2262-SolvingQuestionsWithBrainpower.java
@@ -0,0 +1,18 @@
+// Last updated: 7/31/2025, 6:03:50 PM
+class Solution {
+        public long mostPoints(int[][] questions) {
+                long[] dp = new long[questions.length];
+                        for (int i = questions.length - 1; i >= 0; i--) {
+                                    int index = i + questions[i][1] + 1;
+                                                if (index < questions.length) {
+                                                                dp[i] = dp[index] + questions[i][0];
+                                                                            } else {
+                                                                                            dp[i] = questions[i][0];
+                                                                                                        }
+                                                                                                                    if (i < questions.length - 1) {
+                                                                                                                                    dp[i] = Math.max(dp[i + 1], dp[i]);
+                                                                                                                                                }
+                                                                                                                                                        }
+                                                                                                                                                                return dp[0];
+                                                                                                                                                                    }
+                                                                                                                                                                    }
