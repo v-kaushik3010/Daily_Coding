f662c57134dd9916de7bdc8b6c4fe5dd7190f814
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-07-31 18:02:46 +0530

    Create 3447-ClearDigits

---
+++ 3447-ClearDigits/3447-ClearDigits.java
@@ -0,0 +1,27 @@
+// Last updated: 7/31/2025, 6:02:33 PM
+class Solution {
+
+    public String clearDigits(String s) {
+        int charIndex = 0;
+
+        StringBuilder sb = new StringBuilder(s);
+
+        // Until we reach the end of the string
+        while (charIndex < sb.length()) {
+            if (Character.isDigit(sb.charAt(charIndex))) {
+                // Remove the digit from the string
+                sb.deleteCharAt(charIndex);
+                // If there is a character to the left of the digit, remove it
+                if (charIndex > 0) {
+                    sb.deleteCharAt(charIndex - 1);
+                    // Adjust the index to account for the removed character
+                    charIndex--;
+                }
+            } else {
+                // Move to the next character if it's not a digit
+                charIndex++;
+            }
+        }
+        return sb.toString();
+    }
+}
