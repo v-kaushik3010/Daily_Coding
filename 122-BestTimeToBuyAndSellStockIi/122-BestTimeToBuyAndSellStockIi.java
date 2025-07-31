3129a8423653e1fe5ab50ed543c20c0d55828538
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-07-31 18:05:48 +0530

    Create 122-BestTimeToBuyAndSellStockIi

---
+++ 122-BestTimeToBuyAndSellStockIi/122-BestTimeToBuyAndSellStockIi.java
@@ -0,0 +1,12 @@
+// Last updated: 7/31/2025, 6:05:35 PM
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - prices[i - 1] > 0) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+}
