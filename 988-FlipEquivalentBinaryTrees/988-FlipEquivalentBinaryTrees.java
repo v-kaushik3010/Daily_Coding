6f17cc85c89e8bb4657957a863d209c6be646d57
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-07-31 18:05:01 +0530

    Create 988-FlipEquivalentBinaryTrees

---
+++ 988-FlipEquivalentBinaryTrees/988-FlipEquivalentBinaryTrees.java
@@ -0,0 +1,20 @@
+// Last updated: 7/31/2025, 6:04:48 PM
+
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        
+        return checker(root1, root2);
+    }
+
+    private boolean checker(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null || node1.val != node2.val) {
+            return false;
+        }
+
+        return (checker(node1.left, node2.left) || checker(node1.left, node2.right)) &&
+               (checker(node1.right, node2.right) || checker(node1.right, node2.left));
+    }
+}
