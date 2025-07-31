ebb9e18d75535c42d1187560a5bb9a75a226cef8
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-07-31 18:07:13 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,9 @@
+# Last updated: 7/31/2025, 6:07:00 PM
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        raise ValueError("No two sum solution")
+        
