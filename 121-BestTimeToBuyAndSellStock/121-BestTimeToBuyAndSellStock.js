718fda7b62d32c7d0957abfafbe6e1ddafb9bbb2
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-07-31 18:05:52 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.js
@@ -0,0 +1,15 @@
+// Last updated: 7/31/2025, 6:05:39 PM
+var maxProfit = function(prices) {
+    let buyPrice = prices[0];
+    let profit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (buyPrice > prices[i]) {
+            buyPrice = prices[i];
+        }
+
+        profit = Math.max(profit, prices[i] - buyPrice);
+    }
+
+    return profit;    
+};
