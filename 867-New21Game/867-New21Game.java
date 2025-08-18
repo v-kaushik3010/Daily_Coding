814a1a8d682c6cb4dc086718d127317e06a59bac
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-08-18 20:36:10 +0530

    Create 867-New21Game

---
+++ 867-New21Game/867-New21Game.java
@@ -0,0 +1,28 @@
+// Last updated: 8/18/2025, 8:36:07 PM
+class Solution {
+    public double new21Game(int N, int K, int maxPts) {
+        if (K == 0 || N >= K - 1 + maxPts) return 1.0;
+
+        double[] dp = new double[maxPts]; 
+        dp[0] = 1.0;
+        double windowSum = 1.0, result = 0.0;
+
+        for (int i = 1; i <= N; i++) {
+            double prob = windowSum / maxPts;
+            
+            if (i < K) {
+                windowSum += prob;
+            } else {
+                result += prob;
+            }
+
+            if (i >= maxPts) {
+                windowSum -= dp[i % maxPts];
+            }
+
+            dp[i % maxPts] = prob;
+        }
+
+        return result;
+    }
+}
