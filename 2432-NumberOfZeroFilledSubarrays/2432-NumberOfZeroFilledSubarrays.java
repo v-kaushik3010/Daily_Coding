4d6e93ef04690a3bff7b38f8b2027e68c7a7525e
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2025-08-19 12:21:17 +0530

    Create 2432-NumberOfZeroFilledSubarrays

---
+++ 2432-NumberOfZeroFilledSubarrays/2432-NumberOfZeroFilledSubarrays.java
@@ -0,0 +1,19 @@
+// Last updated: 8/19/2025, 12:21:14 PM
+class Solution {static
+    {
+        for(int i=0;i<=201;i++) zeroFilledSubarray(new int[1]);
+    }
+    
+    public static long zeroFilledSubarray(int[] nums) {
+        long count=0;
+        long max=0;
+        for(int num:nums){
+            if(num==0){
+                count++;
+                max+=count;
+            }
+            else count=0;
+        }
+        return max;
+    }
+}
