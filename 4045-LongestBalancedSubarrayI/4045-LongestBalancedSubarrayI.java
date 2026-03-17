fab62cba5fd073c8272e72c3a6a83339f3325ef9
Varun Kaushik <154615091+v-kaushik3010@users.noreply.github.com>
2026-03-17 11:29:44 +0530

    Create 4045-LongestBalancedSubarrayI

---
+++ 4045-LongestBalancedSubarrayI/4045-LongestBalancedSubarrayI.java
@@ -0,0 +1,19 @@
+// Last updated: 3/17/2026, 11:29:43 AM
+import java.util.*;
+
+class Solution {
+    public int longestBalanced(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            Set<Integer> e = new HashSet<>();
+            Set<Integer> o = new HashSet<>();
+            for (int j = i; j < n; j++) {
+                if (nums[j] % 2 == 0) e.add(nums[j]);
+                else o.add(nums[j]);
+                if (e.size() == o.size()) ans = Math.max(ans, j - i + 1);
+            }
+        }
+        return ans;
+    }
+}
