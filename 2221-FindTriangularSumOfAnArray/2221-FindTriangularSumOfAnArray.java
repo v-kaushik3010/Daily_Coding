e2d66dda6ceb1bdf8ec2c0b536508b3da5351d27
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:48:35 +0530

    Create 2221-FindTriangularSumOfAnArray

---
+++ 2221-FindTriangularSumOfAnArray/2221-FindTriangularSumOfAnArray.java
@@ -0,0 +1,14 @@
+// Last updated: 9/22/2026, 2:48:36 PM
+class Solution {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        
+        for (int size = n; size > 1; size--) {
+            for (int i = 0; i < size - 1; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+        }
+
+        return nums[0];
+    }
+}
