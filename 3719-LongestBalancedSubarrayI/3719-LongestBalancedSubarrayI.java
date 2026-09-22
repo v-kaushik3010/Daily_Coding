3ca9668784cb7575b0ce0de3736acdbb94f19e25
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:42:16 +0530

    Create 3719-LongestBalancedSubarrayI

---
+++ 3719-LongestBalancedSubarrayI/3719-LongestBalancedSubarrayI.java
@@ -0,0 +1,19 @@
+// Last updated: 9/22/2026, 2:42:16 PM
+import java.util.*;
+
+class Solution {
+    public int longestBalanced(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            Set<Integer> e = new HashSet<>();
+            Set<Integer> o = new HashSet<>();
+            for (int j = i; j < n; j++) {
+                if (nums[j] % 2 == 0) e.add(nums[j]);
+                else o.add(nums[j]);
+                if (e.size() == o.size()) ans = Math.max(ans, j - i + 1);
+            }
+        }
+        return ans;
+    }
+}
