3730d6b75bd4b2c7773e8c71e8a16d08e0aaadb6
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:44:05 +0530

    Create 3307-FindTheKThCharacterInStringGameIi

---
+++ 3307-FindTheKThCharacterInStringGameIi/3307-FindTheKThCharacterInStringGameIi.java
@@ -0,0 +1,19 @@
+// Last updated: 9/22/2026, 2:44:05 PM
+class Solution {
+
+    public char kthCharacter(long k, int[] operations) {
+        int ans = 0;
+        int t;
+        while (k != 1) {
+            t = 63 - Long.numberOfLeadingZeros(k);
+            if ((1L << t) == k) {
+                t--;
+            }
+            k = k - (1L << t);
+            if (operations[t] != 0) {
+                ans++;
+            }
+        }
+        return (char) ('a' + (ans % 26));
+    }
+}
