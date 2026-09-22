28eddb388ceb9d3ba717b88d278a36a87536f724
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:53:39 +0530

    Create 2226-MaximumCandiesAllocatedToKChildren

---
+++ 2226-MaximumCandiesAllocatedToKChildren/2226-MaximumCandiesAllocatedToKChildren.java
@@ -0,0 +1,34 @@
+// Last updated: 9/22/2026, 2:53:39 PM
+class Solution {
+
+    public boolean checkvalid(int []candie,int val,long m){
+    long count=0;
+for(int k=0;k<candie.length;k++){
+  count=count+candie[k]/val;
+}
+return count>=m;
+}
+
+    public int maximumCandies(int[] candies, long k) {
+        
+long sum=0;
+int min=1;
+for(int i=0;i<candies.length;i++){
+   sum=sum+candies[i];
+}
+if(sum<k){
+    return 0;
+}
+int max=(int) (sum/k);
+        while(min<=max){
+            int mid = (min+max)/2;
+            if(checkvalid(candies,mid,k)){
+                min=mid+1; 
+            }
+            else{
+                max=mid-1;
+            }
+        }
+        return max;
+    }
+}
