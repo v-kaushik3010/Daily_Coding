048b7632ac820b9e4b430fb2adeb5d913892c0f9
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:50:36 +0530

    Create 1769-MinimumNumberOfOperationsToMoveAllBallsToEachBox

---
+++ 1769-MinimumNumberOfOperationsToMoveAllBallsToEachBox/1769-MinimumNumberOfOperationsToMoveAllBallsToEachBox.java
@@ -0,0 +1,20 @@
+// Last updated: 9/22/2026, 2:50:37 PM
+class Solution {
+
+    public int[] minOperations(String boxes) {
+        int[] answer = new int[boxes.length()];
+        for (int currentBox = 0; currentBox < boxes.length(); currentBox++) {
+            // If the current box contains a ball, calculate the number of moves for every box.
+            if (boxes.charAt(currentBox) == '1') {
+                for (
+                    int newPosition = 0;
+                    newPosition < boxes.length();
+                    newPosition++
+                ) {
+                    answer[newPosition] += Math.abs(newPosition - currentBox);
+                }
+            }
+        }
+        return answer;
+    }
+}
