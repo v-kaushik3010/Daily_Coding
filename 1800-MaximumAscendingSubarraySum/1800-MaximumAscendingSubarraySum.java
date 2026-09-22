261dd821ae8137084a2433a5d144cc97b11ad638
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:50:20 +0530

    Create 1800-MaximumAscendingSubarraySum

---
+++ 1800-MaximumAscendingSubarraySum/1800-MaximumAscendingSubarraySum.java
@@ -0,0 +1,26 @@
+// Last updated: 9/22/2026, 2:50:21 PM
+class Solution {
+
+    public int maxAscendingSum(int[] nums) {
+        int maxSum = 0;
+
+        // Outer loop to start from each element in the array
+        for (int startIdx = 0; startIdx < nums.length; startIdx++) {
+            int currentSubarraySum = nums[startIdx];
+
+            // Inner loop to check the next elements forming an ascending subarray
+            for (
+                int endIdx = startIdx + 1;
+                endIdx < nums.length && nums[endIdx] > nums[endIdx - 1];
+                endIdx++
+            ) {
+                currentSubarraySum += nums[endIdx];
+            }
+
+            // Update maxSum if we find a larger ascending subarray sum
+            maxSum = Math.max(maxSum, currentSubarraySum);
+        }
+
+        return maxSum;
+    }
+}
