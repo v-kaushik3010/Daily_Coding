04b788e00d9f3304cfd74dd1f0c31fddcd822638
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:47:55 +0530

    Create 2401-LongestNiceSubarray

---
+++ 2401-LongestNiceSubarray/2401-LongestNiceSubarray.java
@@ -0,0 +1,21 @@
+// Last updated: 9/22/2026, 2:47:55 PM
+class Solution {
+        public int longestNiceSubarray(int[] nums) {
+                int n = nums.length;
+                        int maxLength = 1;
+                                int left = 0;
+                                        int usedBits = 0;
+                                                
+                                                        for (int right = 0; right < n; right++) {
+                                                                    while ((usedBits & nums[right]) != 0) {
+                                                                                    usedBits ^= nums[left];
+                                                                                                    left++;
+                                                                                                                }
+                                                                                                                            
+                                                                                                                                        usedBits |= nums[right];
+                                                                                                                                                    maxLength = Math.max(maxLength, right - left + 1);
+                                                                                                                                                            }
+                                                                                                                                                                    
+                                                                                                                                                                            return maxLength;
+                                                                                                                                                                                }
+                                                                                                                                                                                }
