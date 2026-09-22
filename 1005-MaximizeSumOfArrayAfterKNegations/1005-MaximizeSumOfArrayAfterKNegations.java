546070ce1bab0d68bc01fadf492576609a8920e3
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:54:40 +0530

    Create 1005-MaximizeSumOfArrayAfterKNegations

---
+++ 1005-MaximizeSumOfArrayAfterKNegations/1005-MaximizeSumOfArrayAfterKNegations.java
@@ -0,0 +1,18 @@
+// Last updated: 9/22/2026, 2:54:40 PM
+class Solution {
+   public int largestSumAfterKNegations(int[] A, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i : A){
+            pq.add(i);
+            
+        }
+        while(K-->0){
+            pq.add(-pq.poll());
+        }
+        int sum =0;
+        while(!pq.isEmpty()){
+            sum+=pq.poll();
+        }
+        return sum;
+    }
+}
