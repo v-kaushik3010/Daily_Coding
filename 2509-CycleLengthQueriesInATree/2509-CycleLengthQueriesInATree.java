58bdbb42c9f0c40022bc688f957696f2256f12b1
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:47:13 +0530

    Create 2509-CycleLengthQueriesInATree

---
+++ 2509-CycleLengthQueriesInATree/2509-CycleLengthQueriesInATree.java
@@ -0,0 +1,18 @@
+// Last updated: 9/22/2026, 2:47:14 PM
+class Solution {
+    public int solve(int x, int y){
+        int count = 1;
+        while(x!= y){
+            if(x>y) x /=2;
+            else y/=2;
+            count++;
+        }return count;
+    }
+    public int[] cycleLengthQueries(int n, int[][] queries) {
+        int len = queries.length;
+        int[] ans = new int[len];
+        for(int i = 0;i < len; i++){
+            ans[i] = solve(queries[i][0], queries[i][1]);
+        }return ans;
+    }
+}
