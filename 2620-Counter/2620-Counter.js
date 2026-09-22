b7b92fe2c62adc7509f17d98574506ade6eaee49
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:46:34 +0530

    Create 2620-Counter

---
+++ 2620-Counter/2620-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 9/22/2026, 2:46:35 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
