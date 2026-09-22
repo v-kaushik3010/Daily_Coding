32bba0a20680c7fcc9383fda9c4d626ab8453f8c
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:43:50 +0530

    Create 3375-MinimumOperationsToMakeArrayValuesEqualToK

---
+++ 3375-MinimumOperationsToMakeArrayValuesEqualToK/3375-MinimumOperationsToMakeArrayValuesEqualToK.java
@@ -0,0 +1,18 @@
+// Last updated: 9/22/2026, 2:43:50 PM
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int mini = nums[0];
+        if (mini < k) return -1;
+
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > k) {
+                cnt++;
+                while (i + 1 < nums.length && nums[i] == nums[i + 1]) 
+                    i++;
+            }
+        }
+        return cnt;
+    }
+}
