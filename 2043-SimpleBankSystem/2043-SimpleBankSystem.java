d25a999213f9c01ee70a27f8fdc12a9459860687
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:49:31 +0530

    Create 2043-SimpleBankSystem

---
+++ 2043-SimpleBankSystem/2043-SimpleBankSystem.java
@@ -0,0 +1,38 @@
+// Last updated: 9/22/2026, 2:49:31 PM
+class Bank {
+    long[] balance;
+    int n;
+
+    public Bank(long[] balance) {
+        this.balance = balance;
+        this.n = balance.length;
+    }
+
+    public boolean transfer(int account1, int account2, long money) {
+        if (account1 < 1 || account1 > n ||
+            account2 < 1 || account2 > n ||
+            balance[account1 - 1] < money) {
+            return false;
+        }
+
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    public boolean deposit(int account, long money) {
+        if (account < 1 || account > n) return false;
+
+        balance[account - 1] += money;   // ✅ fixed
+        return true;
+    }
+
+    public boolean withdraw(int account, long money) {
+        if (account < 1 || account > n || balance[account - 1] < money) {
+            return false;
+        }
+
+        balance[account - 1] -= money;   // ✅ fixed
+        return true;
+    }
+}
