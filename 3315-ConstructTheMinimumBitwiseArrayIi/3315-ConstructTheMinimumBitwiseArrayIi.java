937dfe2fc58db00ee252c1ca914addb424afb121
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:44:02 +0530

    Create 3315-ConstructTheMinimumBitwiseArrayIi

---
+++ 3315-ConstructTheMinimumBitwiseArrayIi/3315-ConstructTheMinimumBitwiseArrayIi.java
@@ -0,0 +1,12 @@
+// Last updated: 9/22/2026, 2:44:02 PM
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int ans[] = new int[nums.size()];
+        for(int i = 0; i < nums.size(); i++) {
+            int n = nums.get(i);
+            if(n != 2) ans[i] = n - ((n + 1) & (-n - 1)) / 2;
+            else ans[i] = -1;
+        }  
+        return ans;
+    }
+}
