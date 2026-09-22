9a1c41f65b76ecde0ce3584ae496950edab27a65
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:52:03 +0530

    Create 1510-StoneGameIv

---
+++ 1510-StoneGameIv/1510-StoneGameIv.java
@@ -0,0 +1,16 @@
+// Last updated: 9/22/2026, 2:52:03 PM
+class Solution {
+    static final int MAX = 100000;
+    static final boolean[] dp = new boolean[MAX + 1];
+
+    static {
+        for (int i = 0; i <= MAX; i++) {
+            if (dp[i]) continue;
+
+            for (int j = 1; j * j <= MAX - i; j++)
+                dp[i + j * j] = true;
+        }
+    }
+
+    public boolean winnerSquareGame(int n) { return dp[n]; }
+}
