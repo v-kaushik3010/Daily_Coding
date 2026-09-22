7fad8e7d9614e4b6dd3d7f29900138924147ba16
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:46:55 +0530

    Create 2579-CountTotalNumberOfColoredCells

---
+++ 2579-CountTotalNumberOfColoredCells/2579-CountTotalNumberOfColoredCells.java
@@ -0,0 +1,16 @@
+// Last updated: 9/22/2026, 2:46:55 PM
+class Solution {
+
+    public long coloredCells(int n) {
+        long numBlueCells = 1;
+        int addend = 4;
+
+        // Iterate n - 1 times
+        while (--n > 0) {
+            // Add and update current multiple of 4
+            numBlueCells += addend;
+            addend += 4;
+        }
+        return numBlueCells;
+    }
+}
