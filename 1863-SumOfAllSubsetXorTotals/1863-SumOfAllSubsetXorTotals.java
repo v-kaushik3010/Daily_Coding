05ab1456137dca9e1451d0d8a2995a8fb3cd68db
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:50:08 +0530

    Create 1863-SumOfAllSubsetXorTotals

---
+++ 1863-SumOfAllSubsetXorTotals/1863-SumOfAllSubsetXorTotals.java
@@ -0,0 +1,19 @@
+// Last updated: 9/22/2026, 2:50:08 PM
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int n = nums.length;
+        int totalSum = 0;
+        // Iterate through all possible subsets
+        for (int i = 0; i < (1 << n); i++) {
+            int subsetXor = 0;
+            for (int j = 0; j < n; j++) {
+                // Check if the j-th element is in the i-th subset
+                if ((i & (1 << j)) != 0) {
+                    subsetXor ^= nums[j];
+                }
+            }
+            totalSum += subsetXor;
+        }
+        return totalSum;
+    }
+}
