724233fc0f73b2bd3266905409c7c36a427eb780
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:47:44 +0530

    Create 2411-SmallestSubarraysWithMaximumBitwiseOr

---
+++ 2411-SmallestSubarraysWithMaximumBitwiseOr/2411-SmallestSubarraysWithMaximumBitwiseOr.java
@@ -0,0 +1,24 @@
+// Last updated: 9/22/2026, 2:47:44 PM
+class Solution {
+
+    public int[] smallestSubarrays(int[] nums) {
+        int n = nums.length;
+        int[] pos = new int[31];
+        Arrays.fill(pos, -1);
+        int[] ans = new int[n];
+        for (int i = n - 1; i >= 0; --i) {
+            int j = i;
+            for (int bit = 0; bit < 31; ++bit) {
+                if ((nums[i] & (1 << bit)) == 0) {
+                    if (pos[bit] != -1) {
+                        j = Math.max(j, pos[bit]);
+                    }
+                } else {
+                    pos[bit] = i;
+                }
+            }
+            ans[i] = j - i + 1;
+        }
+        return ans;
+    }
+}
