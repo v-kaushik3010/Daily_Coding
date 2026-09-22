0604118b4baba2f3331440dd5c8cd4d85f4fb503
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:45:47 +0530

    Create 2873-MaximumValueOfAnOrderedTripletI

---
+++ 2873-MaximumValueOfAnOrderedTripletI/2873-MaximumValueOfAnOrderedTripletI.java
@@ -0,0 +1,12 @@
+// Last updated: 9/22/2026, 2:45:47 PM
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        long maxTriplet = 0, maxElement = 0, maxDiff = 0;
+        for (int num : nums) {
+            maxTriplet = Math.max(maxTriplet, maxDiff * num);
+            maxDiff = Math.max(maxDiff, maxElement - num);
+            maxElement = Math.max(maxElement, num);
+        }
+        return maxTriplet;
+    }
+}
