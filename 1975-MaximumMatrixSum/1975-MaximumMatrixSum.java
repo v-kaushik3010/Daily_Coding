1d29a8f7b705a575746e4e876b2e0f6782a26d05
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:49:49 +0530

    Create 1975-MaximumMatrixSum

---
+++ 1975-MaximumMatrixSum/1975-MaximumMatrixSum.java
@@ -0,0 +1,23 @@
+// Last updated: 9/22/2026, 2:49:49 PM
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        int mini = Integer.MAX_VALUE;
+        int count=0;
+        int n = matrix.length;
+        long sum = 0;
+        for(int i =0 ; i < n; i++){
+            for(int j = 0; j<n; j++){
+                if(matrix[i][j]>0) {
+                    sum+=(long)matrix[i][j];
+                    mini= Math.min(mini, matrix[i][j]);
+                    }
+                else{
+                    sum -=(long)matrix[i][j];
+                    count++;
+                    mini= Math.min(mini, -matrix[i][j]);
+                }
+
+            }
+        }return count%2==0?sum: (sum -(long)(2*mini));
+    }
+}
