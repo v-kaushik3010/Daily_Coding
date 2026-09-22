ddf087e7b60fa8eebb8240c5ae6f5a6930c69c09
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:46:50 +0530

    Create 2551-PutMarblesInBags

---
+++ 2551-PutMarblesInBags/2551-PutMarblesInBags.java
@@ -0,0 +1,23 @@
+// Last updated: 9/22/2026, 2:46:51 PM
+class Solution {
+        public long putMarbles(int[] weights, int k) {
+                if (k == 1) {
+                            return 0;
+                                    }
+
+                                            List<Integer> pairSums = new ArrayList<>();
+                                                    for (int i = 0; i < weights.length - 1; i++) {
+                                                                pairSums.add(weights[i] + weights[i + 1]);
+                                                                        }
+
+                                                                                Collections.sort(pairSums);
+
+                                                                                        long minScore = 0, maxScore = 0;
+                                                                                                for (int i = 0; i < k - 1; i++) {
+                                                                                                            minScore += pairSums.get(i);
+                                                                                                                        maxScore += pairSums.get(pairSums.size() - 1 - i);
+                                                                                                                                }
+
+                                                                                                                                        return maxScore - minScore;
+                                                                                                                                            }
+                                                                                                                                            }
