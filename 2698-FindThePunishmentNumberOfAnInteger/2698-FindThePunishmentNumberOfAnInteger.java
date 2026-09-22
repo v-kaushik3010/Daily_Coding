3be61aee1624f3b01c30f059fc98a4a565dcde34
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:46:19 +0530

    Create 2698-FindThePunishmentNumberOfAnInteger

---
+++ 2698-FindThePunishmentNumberOfAnInteger/2698-FindThePunishmentNumberOfAnInteger.java
@@ -0,0 +1,17 @@
+// Last updated: 9/22/2026, 2:46:19 PM
+class Solution {
+    int arr[] = { 1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657, 675, 703, 756, 792, 909,
+            918, 945, 964, 990, 991, 999, 1000 };
+
+    public int punishmentNumber(int n) {
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= n) {
+                res += arr[i] * arr[i];
+            } else {
+                break;
+            }
+        }
+        return res;
+    }
+}
