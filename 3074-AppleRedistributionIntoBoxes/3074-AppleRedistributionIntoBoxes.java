945f5e84e8d52bcbcea278a3dddace61a23d9c73
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:44:55 +0530

    Create 3074-AppleRedistributionIntoBoxes

---
+++ 3074-AppleRedistributionIntoBoxes/3074-AppleRedistributionIntoBoxes.java
@@ -0,0 +1,16 @@
+// Last updated: 9/22/2026, 2:44:55 PM
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int apples = 0;
+        for(int i : apple) apples += i;
+        Arrays.sort(capacity);
+        int reqbox =0;
+        int count =0;;
+        for(int i = capacity.length -1; i >= 0; i--){
+            count++;
+            reqbox+=capacity[i];
+            if(reqbox >=apples) return count;
+        }return -1;
+        
+    }
+}
