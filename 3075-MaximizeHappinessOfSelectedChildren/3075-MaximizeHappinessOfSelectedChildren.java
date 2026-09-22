0f3c3fc1ff2926611f268e83279d9856314892ad
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-09-22 14:44:50 +0530

    Create 3075-MaximizeHappinessOfSelectedChildren

---
+++ 3075-MaximizeHappinessOfSelectedChildren/3075-MaximizeHappinessOfSelectedChildren.java
@@ -0,0 +1,17 @@
+// Last updated: 9/22/2026, 2:44:51 PM
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        int n = happiness.length;
+        int i =0;
+        long sum =0;
+        while(k-- >0){
+            long val =happiness[n-1-i] -i; 
+            if( val>0 ){
+                sum+=val;
+                i++;
+            }
+        }return sum;
+        
+    }
+}
